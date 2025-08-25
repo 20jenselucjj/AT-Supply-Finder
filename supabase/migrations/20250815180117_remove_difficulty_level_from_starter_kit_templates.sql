c3de5d32574e7e5d1c4dc72b9632fc11ce4253ae
20jenselucjj <20jenselucjj@gmail.com>
2025-08-25 16:55:50 -0600

    deleted:    supabase/migrations/20250815180117_remove_difficulty_level_from_starter_kit_templates.sql 	deleted:    supabase/migrations/20250815180200_remove_estimated_time_from_starter_kit_templates.sql 	deleted:    supabase/migrations/20250815180300_create_admin_functions.sql 	deleted:    supabase/migrations/20250815180400_fix_database_schema.sql 	deleted:    supabase/migrations/20250815180500_fix_enum_issues.sql

--- supabase/migrations/20250815180117_remove_difficulty_level_from_starter_kit_templates.sql
+++
@@ -1,3 +0,0 @@
--- Remove difficulty_level column from starter_kit_templates table
-ALTER TABLE IF EXISTS public.starter_kit_templates 
-DROP COLUMN IF EXISTS difficulty_level;
--- supabase/migrations/20250815180200_remove_estimated_time_from_starter_kit_templates.sql
+++
@@ -1,3 +0,0 @@
--- Remove estimated_time column from starter_kit_templates table
-ALTER TABLE IF EXISTS public.starter_kit_templates 
-DROP COLUMN IF EXISTS estimated_time;
